HighGoal1991/go-ethereum

commit 4cac077e5dd44204965446103aa799f799c6f2fc

internal/web3ext: fix eth_call stateOverrides in console (#26265)
web3.js's eth_call which we were defaulting to doesn't have the stateOverrides parameter, so this param wasn't working in the console.

internal/web3ext/web3ext.go
@@ -600,6 +600,11 @@ web3._extend({
 			call: 'eth_getLogs',
 			params: 1,
 		}),
+		new web3._extend.Method({
+			name: 'call',
+			call: 'eth_call',
+			params: 3,
+		}),
 	],
 	properties: [
 		new web3._extend.Property({

commit 32f532c8366a58f139be190f6107e94de549fed8

internal/web3ext: fix eth.call regression in console (#26266)
Fixes a regression from #26265, which made it so that the call only worked if all three parameters were provided.

internal/web3ext/web3ext.go
@@ -604,6 +604,7 @@ web3._extend({
 			name: 'call',
 			call: 'eth_call',
 			params: 3,
+			inputFormatter: [formatters.inputCallFormatter, formatters.inputDefaultBlockNumberFormatter, null],
 		}),
 	],
 	properties: [